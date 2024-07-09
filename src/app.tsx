d467e29e73c12e5b03b58f7a78245145a7a20d87
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-07-09 11:22:38 -0300

    feat: add home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,52 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react"
+
 export const App = () => {
-  return <div className="bg-zinc-100 text-red-500">Hello World</div>;
-};
+  return (
+    <div className="bg-pattern flex h-screen items-center justify-center bg-center bg-no-repeat">
+      <div className="w-full max-w-3xl space-y-10 px-6 text-center">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="Logo plann.er" />
+          <p className="text-lg text-zinc-300">
+            Convide seus amigos e planeje sua próxima viagem
+          </p>
+        </div>
+        <div className="shadow-shape flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4">
+          <div className="flex flex-1 items-center gap-2">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="w-full bg-transparent text-lg outline-none placeholder:text-zinc-400"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="w-40 bg-transparent text-lg outline-none placeholder:text-zinc-400"
+            />
+          </div>
+          <div className="h-6 w-px bg-zinc-800" />
+          <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 transition-colors hover:bg-lime-400">
+            Continuar
+            <ArrowRight size={20} />
+          </button>
+        </div>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda
+          <br />
+          com nossos{" "}
+          <a href="#" className="text-zinc-300 underline">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a href="#" className="text-zinc-300 underline">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  )
+}
